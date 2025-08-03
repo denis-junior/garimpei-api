f0b2daed77b91805a8c04f8da138b796fd808b65
denis junior <denis_jr2001@hotmail.com>
2025-08-03 20:35:50 -0300

    feat(data-source): remove default database configuration in favor of environment variables

--- src/data-source.ts
+++ src/data-source.ts
@@ -5,11 +5,11 @@ dotenv.config();
 
 export const AppDataSource = new DataSource({
   type: 'postgres',
-  host: process.env.DB_HOST || 'localhost',
-  port: parseInt(process.env.DB_PORT || '7000'),
-  username: process.env.DB_USERNAME || 'postgres',
-  password: process.env.DB_PASSWORD || 'postgres',
-  database: process.env.DB_DATABASE || 'garimpeidb',
+  host: process.env.DB_HOST,
+  port: parseInt(process.env.DB_PORT),
+  username: process.env.DB_USERNAME,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_DATABASE,
   entities: ['src/**/*.entity.ts'],
   migrations: ['src/migrations/*.ts'],
   synchronize: false,
