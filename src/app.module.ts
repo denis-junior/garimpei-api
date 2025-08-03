519714d166f3abd7f882364f64549d0e3f8fcffb
denis junior <denis_jr2001@hotmail.com>
2025-08-03 20:01:16 -0300

    feat(app): configure global environment variables and update TypeORM connection settings

--- src/app.module.ts
+++ src/app.module.ts
@@ -21,19 +21,27 @@ import { AuthModule } from './auth/auth.module';
 // import { WebhooksModule } from './webhooks/webhooks.module';
 import { ScheduleModule } from '@nestjs/schedule';
 import { EmailModule } from './email/email.module';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 @Module({
   imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: '.env',
+    }),
     ScheduleModule.forRoot(),
-    TypeOrmModule.forRoot({
-      type: 'postgres',
-      host: 'localhost',
-      port: 7000,
-      username: 'postgres',
-      password: 'root',
-      database: 'garimpeidb',
-      entities: [Buyer, Seller, Clothing, Bid, Store, Image],
-      synchronize: true, // true só para desenvolvimento!
+    TypeOrmModule.forRootAsync({
+      useFactory: (configService: ConfigService) => ({
+        type: 'postgres',
+        host: configService.get('DB_HOST'),
+        port: configService.get('DB_PORT'),
+        username: configService.get('DB_USERNAME'),
+        password: configService.get('DB_PASSWORD'),
+        database: configService.get('DB_DATABASE'),
+        synchronize: true,
+        entities: [Buyer, Seller, Clothing, Bid, Store, Image],
+      }),
+      inject: [ConfigService],
     }),
     BuyerModule,
     SellerModule,
